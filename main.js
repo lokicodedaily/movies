e1f0c5e50a2a521d0ee63ecde69f57823eefb4dc
lokicodedaily <you@example.com>
2023-07-12 15:56:35 +0530

    add event listener to every movie card

--- main.js
+++ main.js
@@ -2,10 +2,11 @@ const seachInputElement = document.getElementById("search-bar");
 const searchBtn = document.getElementById("search-btn");
 const moviesSection = document.getElementById("movies-section");
 
+
 // to get the data from ombd api
 async function getUser(movieName = "spiderman") {
   try {
-    movieName = movieName === ''? 'Spiderman': movieName;
+    movieName = movieName === "" ? "Spiderman" : movieName;
     const response = await axios.get(
       `http://www.omdbapi.com/?s=${movieName}&page=1&apikey=a66eb111`
     );
@@ -13,33 +14,38 @@ async function getUser(movieName = "spiderman") {
     displayMovies(data);
   } catch (error) {
     console.log(error);
-    
   }
 }
-
 
 // function to diplay movie in form of card
 function displayMovies(movies) {
   let moviesCards = movies.map((movie) => {
-    let title = movie['Title'];
-    let img = movie["Poster"] === 'N/A'? './moviePoster.jpg': movie['Poster'];
+    let title = movie["Title"];
+    let img = movie["Poster"] === "N/A" ? "./moviePoster.jpg" : movie["Poster"];
 
-    return `<div class="movie-card">
+    return `
           <img src="${img}" alt="movie_name">
           <span>${title}</span>
-        </div>`;
+        `;
   });
 
   moviesSection.innerHTML = "";
   for (let movieCard of moviesCards) {
-    moviesSection.innerHTML += movieCard;
+    let movieCardElement = Object.assign(document.createElement("div"), {
+      className: "movie-card",
+    });
+    movieCardElement.innerHTML = movieCard;
+    movieCardElement.addEventListener("click", () => {
+      console.log(movieCardElement);
+    });
+    moviesSection.appendChild(movieCardElement);
   }
 }
 
 // event listener for seraching
-searchBtn.addEventListener('click', () => {
+searchBtn.addEventListener("click", () => {
   getUser(seachInputElement.value);
-})
+});
 
 // calling to get data from function
 getUser();
